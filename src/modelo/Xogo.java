4546c24fa5f26a60c7f7a9217d09177925afa680
damianld <damilores@gmail.com>
2018-04-06 18:34:34 +0200

    engadir metodos que faltan a clase Xogo

--- src/modelo/Xogo.java
+++ src/modelo/Xogo.java
@@ -8,7 +8,7 @@ public class Xogo {
     private final int LADO_CADRADO = 30;
     private final int MAX_X = 300;
     private final int MAX_Y = 900;
-    private boolean pausa=false;
+    private boolean pausa;
     private int numeroLinas;
     private VentanaPrincipal ventanaPrincipal;
     private ArrayList<Cadrado> cadradosChan;
@@ -16,5 +16,45 @@ public class Xogo {
 
     public Xogo (VentanaPrincipal ventanaPrincipal){
         this.ventanaPrincipal = ventanaPrincipal;
+    }
+    
+    public void moverFichaDereita() {
+    	
+    }
+    
+    public void moverFichaEsquerda() {
+    	
+    }
+    
+    public void moverFichaAbaixo() {
+    	
+    }
+    
+    public void rotarFicha() {
+    	
+    }
+    
+    private void ePosicionValida(int x, int y) {
+    	
+    }
+    
+    private void xenerarNovaFicha() {
+    	
+    }
+    
+    private void engadirFichaAoChan() {
+    	
+    }
+    
+    private void borrarLinasCompletas() {
+    	
+    }
+    
+    private void borrarLina() {
+    	
+    }
+    
+    private boolean chocaFichaCoChan() {
+    	return false;
     }
 }
