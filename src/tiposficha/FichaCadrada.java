9161c5a10a80fa1cf1102a3bc475da47fe515cca
damianld <damilores@gmail.com>
2018-04-14 05:34:07 +0200

    Incluido metodo para mover dereita autocalculase as cordenadas para que a ficha apareza no medio

--- src/tiposficha/FichaCadrada.java
+++ src/tiposficha/FichaCadrada.java
@@ -5,43 +5,50 @@ import java.util.ArrayList;
 
 import modelo.Cadrado;
 import modelo.Ficha;
+import modelo.Xogo;
 
 public class FichaCadrada extends Ficha {
 
-	public FichaCadrada() {
-		super.setCadrados(xerarCadrados());
-	}
+    public FichaCadrada() {
+        super.setCadrados(xerarCadrados());
+    }
 
-	@Override
-	public boolean moverDereita() {
-		return false;
-	}
+    @Override
+    public boolean moverDereita() {
+        for (Cadrado cadrado : super.getCadrados()) {
+            System.out.println(cadrado.getCoordenadas());
+            cadrado.setX(cadrado.getX() + Xogo.LADO_CADRADO);
+            System.out.println(cadrado.getCoordenadas());
+        }
+        System.out.println("Ficha movida a dereita");
+        return true;
+    }
 
-	@Override
-	public boolean moverEsquerda() {
-		return false;
-	}
+    @Override
+    public boolean moverEsquerda() {
+        return false;
+    }
 
-	@Override
-	public boolean moverAbaixo() {
-		return false;
-	}
+    @Override
+    public boolean moverAbaixo() {
+        return false;
+    }
 
-	@Override
-	public boolean rotar() {
-		return false;
-	}
+    @Override
+    public boolean rotar() {
+        return false;
+    }
 
-	private ArrayList<Cadrado> xerarCadrados(){
-		Cadrado cadrado0 = new Cadrado(172, 11, Color.BLUE);
-		Cadrado cadrado1 = new Cadrado(300, 100, Color.BLUE);
-		Cadrado cadrado2 = new Cadrado(400, 200, Color.BLUE);
-		Cadrado cadrado3 = new Cadrado(0, 0, Color.BLUE);
-		ArrayList<Cadrado> cadrados = new ArrayList<>();
-		cadrados.add(cadrado0);
-		cadrados.add(cadrado1);
-		cadrados.add(cadrado2);
-		cadrados.add(cadrado3);
-		return cadrados;
-	}
+    private ArrayList<Cadrado> xerarCadrados() {
+        Cadrado cadrado0 = new Cadrado(Xogo.MAX_X / 2, 0, Color.BLUE);
+        Cadrado cadrado1 = new Cadrado(Xogo.MAX_X / 2, Xogo.LADO_CADRADO, Color.BLUE);
+        Cadrado cadrado2 = new Cadrado(Xogo.MAX_X / 2 - Xogo.LADO_CADRADO, 0, Color.BLUE);
+        Cadrado cadrado3 = new Cadrado(Xogo.MAX_X / 2 - Xogo.LADO_CADRADO, Xogo.LADO_CADRADO, Color.BLUE);
+        ArrayList<Cadrado> cadrados = new ArrayList<>();
+        cadrados.add(cadrado0);
+        cadrados.add(cadrado1);
+        cadrados.add(cadrado2);
+        cadrados.add(cadrado3);
+        return cadrados;
+    }
 }
