30546b951c95993a8535ee641044705db8fe8915
damianld <damilores@gmail.com>
2018-05-15 19:15:50 +0200

    Varios tipos de fichas engadidas

---
+++ src/tiposficha/FichaS.java
@@ -0,0 +1,62 @@
+package tiposficha;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import modelo.Cadrado;
+import modelo.Ficha;
+import modelo.Xogo;
+
+public class FichaS extends Ficha {
+	
+	public FichaS() {
+		super.setCadrados(xerarCadrados());
+	}
+
+	@Override
+	public boolean moverDereita() {
+		for (Cadrado cadrado : super.getCadrados()) {
+            cadrado.setX(cadrado.getX() + Xogo.LADO_CADRADO);
+        }
+        return true;
+	}
+
+	@Override
+	public boolean moverEsquerda() {
+		 for (Cadrado cadrado : super.getCadrados()) {
+	            cadrado.setX(cadrado.getX() - Xogo.LADO_CADRADO);
+	        }
+	       return true;
+	}
+
+	@Override
+	public boolean moverAbaixo() {
+		for (Cadrado cadrado : super.getCadrados()) {
+            cadrado.setY(cadrado.getY() + Xogo.LADO_CADRADO);
+        }
+        return false;
+	}
+
+	@Override
+	public boolean rotar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private ArrayList<Cadrado> xerarCadrados() {
+		int ladoDereito = Xogo.MAX_X / 2 - Xogo.LADO_CADRADO;
+		int ladoEsquerdo = Xogo.MAX_X / 2 - Xogo.LADO_CADRADO *2;
+		int ladoArriba = 0;
+		int ladoAbaixo = Xogo.LADO_CADRADO;
+        ArrayList<Cadrado> cadrados = new ArrayList<>();
+        Cadrado cadrado0 = new Cadrado(ladoEsquerdo, ladoAbaixo, Color.GREEN, getXogo());
+        Cadrado cadrado1 = new Cadrado(ladoDereito, ladoAbaixo, Color.GREEN, getXogo());
+        Cadrado cadrado2 = new Cadrado(ladoDereito, ladoArriba, Color.GREEN, getXogo());
+        Cadrado cadrado3 = new Cadrado(ladoDereito + Xogo.LADO_CADRADO, ladoArriba, Color.GREEN, getXogo());
+        cadrados.add(cadrado0);
+        cadrados.add(cadrado1);
+        cadrados.add(cadrado2);
+        cadrados.add(cadrado3);
+        return cadrados;
+	}
+}
