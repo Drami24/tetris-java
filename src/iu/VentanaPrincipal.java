cf0f5aae565b223bc104eb3c73c81cb33d4557f9
damianld <damilores@gmail.com>
2018-05-20 17:23:47 +0200

    Cambios en VentanaPrincipal

--- src/iu/VentanaPrincipal.java
+++ src/iu/VentanaPrincipal.java
@@ -80,8 +80,8 @@ public class VentanaPrincipal {
 	private void initialize() {
 		frame = new JFrame();
 		asignarPropiedadesDoFrame(frame);
-		crearPanelXogo();
 		JPanel contentPane = engadirContentPane(frame);
+		crearPanelXogo();
 		contentPane.add(panelXogo, BorderLayout.CENTER);
 		crearBotonsControlXogo();
 		engadirPanelTetris();
@@ -115,7 +115,7 @@ public class VentanaPrincipal {
 
 	private JPanel engadirContentPane(JFrame frame) {
 		JPanel contentPane = new JPanel();
-		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		frame.setContentPane(contentPane);
@@ -126,23 +126,97 @@ public class VentanaPrincipal {
 	 * Establece as propiedades do frame
 	 * 
 	 * @param frame
-	 *            no cal asignamos as propiedades
+	 *            Frame onde afectan as propiedades
 	 */
 	private void asignarPropiedadesDoFrame(JFrame frame) {
-		frame.setBounds(900, 0, 400, 740);
+		frame.setBounds(600, 50, 550, 660);
 		frame.setTitle("Tetris by @damianld");
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	/**
+	 * Este método é o encargado de de crear o panelXogo
 	 * 
-	 * @return
+	 * @return panelXogo coas súas propiedades
 	 */
 	private JPanel crearPanelXogo() {
 		panelXogo = new JPanel();
-		panelXogo.setFocusable(true);
-		panelXogo.addKeyListener(new KeyAdapter() {
+		panelXogo.setLayout(null);
+		panelXogo.setBackground(Color.GRAY);
+		panelXogo.add(crearBotonNovaPartida());
+		panelXogo.add(crearBotonPausa());
+		panelXogo.setVisible(true);
+		return panelXogo;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JButton crearBotonNovaPartida() {
+		JButton btnNovaPartida = new JButton("Nova Partida");
+		btnNovaPartida.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				iniciarPartida();
+			}
+		});
+		btnNovaPartida.setBounds(324, 12, 150, 23);
+		btnNovaPartida.setFocusable(false);
+		return btnNovaPartida;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JToggleButton crearBotonPausa() {
+		tglbtnPausa = new JToggleButton("Pausa");
+		tglbtnPausa.setBounds(324, 47, 150, 23);
+		tglbtnPausa.setFocusable(false);
+		return tglbtnPausa;
+	}
+
+	private void crearBotonsControlXogo() {
+		JButton btnEsquerda = new JButton("Esquerda");
+		btnEsquerda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				xogoActual.moverFichaEsquerda();
+			}
+		});
+		btnEsquerda.setBounds(324, 473, 89, 23);
+		panelXogo.add(btnEsquerda);
+
+		JButton btnDereita = new JButton("Dereita");
+		btnDereita.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				xogoActual.moverFichaDereita();
+			}
+		});
+		btnDereita.setBounds(425, 473, 89, 23);
+		panelXogo.add(btnDereita);
+
+		JButton btnAbaixo = new JButton("Abaixo");
+		btnAbaixo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				xogoActual.moverFichaAbaixo();
+			}
+		});
+		btnAbaixo.setBounds(324, 511, 89, 23);
+		panelXogo.add(btnAbaixo);
+
+		JButton btnRotar = new JButton("Rotar");
+		btnRotar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnRotar.setBounds(425, 508, 89, 23);
+		panelXogo.add(btnRotar);
+	}
+
+	private void engadirPanelTetris() {
+		panelTetris = new JPanel();
+		panelTetris.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
@@ -160,72 +234,9 @@ public class VentanaPrincipal {
 				}
 			}
 		});
-		panelXogo.setBackground(Color.LIGHT_GRAY);
-		panelXogo.setLayout(null);
-		panelXogo.add(crearBotonNovaPartida());
-		panelXogo.add(crearBotonPausa());
-		panelXogo.setVisible(true);
-		return panelXogo;
-	}
-
-	private JButton crearBotonNovaPartida() {
-		JButton btnNovaPartida = new JButton("Nova Partida");
-		btnNovaPartida.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				iniciarPartida();
-			}
-		});
-		btnNovaPartida.setBounds(62, 0, 150, 23);
-		btnNovaPartida.setFocusable(false);
-		return btnNovaPartida;
-	}
-
-	private JToggleButton crearBotonPausa() {
-		tglbtnPausa = new JToggleButton("Pausa");
-		tglbtnPausa.setBounds(223, 0, 100, 23);
-		return tglbtnPausa;
-	}
-
-	private void crearBotonsControlXogo() {
-		JButton btnEsquerda = new JButton("Esquerda");
-		btnEsquerda.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				xogoActual.moverFichaEsquerda();
-			}
-		});
-		btnEsquerda.setBounds(73, 651, 89, 23);
-		panelXogo.add(btnEsquerda);
-
-		JButton btnDereita = new JButton("Dereita");
-		btnDereita.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				xogoActual.moverFichaDereita();
-			}
-		});
-		btnDereita.setBounds(199, 651, 89, 23);
-		panelXogo.add(btnDereita);
-
-		JButton btnAbaixo = new JButton("Abaixo");
-		btnAbaixo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				xogoActual.moverFichaAbaixo();
-			}
-		});
-		btnAbaixo.setBounds(73, 685, 89, 23);
-		panelXogo.add(btnAbaixo);
-
-		JButton btnRotar = new JButton("Rotar");
-		btnRotar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnRotar.setBounds(199, 685, 89, 23);
-		panelXogo.add(btnRotar);
-	}
-
-	private void engadirPanelTetris() {
-		panelTetris = new JPanel();
-		panelTetris.setBounds(41, 40, 300, 600);
+		panelTetris.setBackground(Color.DARK_GRAY);
+		panelTetris.setBounds(12, 12, 300, 600);
+		panelTetris.setFocusable(true);
 		panelXogo.add(panelTetris);
 		panelTetris.setLayout(null);
 	}
